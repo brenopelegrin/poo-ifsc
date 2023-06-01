6ac03a4c15b0c12aaa37d6a5b4fb47d021492e0a
brenopelegrin <breno.pelegrin@usp.br>
2023-06-01 10:39:28 -0300

    prototype of main.py

--- projeto2/venv/main.py
+++ projeto2/venv/main.py
@@ -1,3 +1,48 @@
+class Image:
+    def __init__(self):
+        pass
+
+    def thresholding(self, t=127):
+        pass
+    def sgt(self, dt=1):
+        pass
+    def mean(self, k=3):
+        """
+            Applies the mean filter onto an Image object, considering 3x3 neighbourhoods.
+
+            Args:
+                param1: This is the first param.
+                param2: This is a second param.
+
+            Returns:
+                This is a description of what is returned.
+
+            Raises:
+                KeyError: Raises an exception.
+        """
+
+        # save image in path
+        return Image()
+
+    def median(self, k=3):
+        """
+            Applies the median filter onto an Image object, considering 3x3 neighbourhoods.
+
+            Args:
+                param1: This is the first param.
+                param2: This is a second param.
+
+            Returns:
+                This is a description of what is returned.
+
+            Raises:
+                KeyError: Raises an exception.
+        """
+
+        # save image in path
+        return Image()
+
+
 def main():
     return
 
